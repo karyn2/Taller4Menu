b0da66da8b6f33da0b1b638a9145016e0dc67944
karyn2 <karynaceron@gmail.com>
2023-08-11 14:59:28 -0500

    taller creacion proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPaginaComponent } from './error-pagina/error-pagina.component';
+
+const routes: Routes=[
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:ErrorPaginaComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ErrorPaginaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
