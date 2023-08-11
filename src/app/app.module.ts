8208323595adbca268093be98095a2eca129027e
vadelgado <vadelgado@udenar.edu.co>
2023-08-11 15:51:30 -0500

    Menu @vadelgado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { ContactComponent } from './contact/contact.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPaginaComponent } from './error-pagina/error-pagina.component';
+import { AlimentosComponent } from './alimentos/alimentos.component';
+import { AccesoriosComponent } from './accesorios/accesorios.component';
+import { ConcentradoComponent } from './concentrado/concentrado.component';
+import { ComidaHumedaComponent } from './comida-humeda/comida-humeda.component';
+import { ArticulosPaseoComponent } from './articulos-paseo/articulos-paseo.component';
+import { JuguetesComponent } from './juguetes/juguetes.component';
 
 const routes: Routes=[
   {
@@ -18,6 +24,30 @@ const routes: Routes=[
   {
     path:'contact',
     component:ContactComponent
+  },
+  {
+    path:'alimentos',
+    component:AlimentosComponent
+  },
+  {
+    path:'comidaHumeda',
+    component:ComidaHumedaComponent
+  },
+  {
+    path:'concentrado',
+    component:ConcentradoComponent
+  },
+  {
+    path:'accesorios',
+    component:AccesoriosComponent
+  },
+  {
+    path:'articulosPaseo',
+    component:ArticulosPaseoComponent
+  },
+  {
+    path:'juguetes',
+    component:JuguetesComponent
   },
   {
     path:'about',
@@ -41,7 +71,13 @@ const routes: Routes=[
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    ErrorPaginaComponent
+    ErrorPaginaComponent,
+    AlimentosComponent,
+    AccesoriosComponent,
+    ConcentradoComponent,
+    ComidaHumedaComponent,
+    ArticulosPaseoComponent,
+    JuguetesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comida-humeda/comida-humeda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comida-humeda',
+  templateUrl: './comida-humeda.component.html',
+  styleUrls: ['./comida-humeda.component.css']
+})
+export class ComidaHumedaComponent {
+
+}
