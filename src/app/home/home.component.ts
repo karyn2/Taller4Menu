6cdf7127e591c28f867bbd85a367098cf17d5f21
karyn2 <karynaceron@gmail.com>
2023-08-12 16:38:52 -0500

    inicio @karyn2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  images: string[] = [ // Array de imágenes
+  '/assets/img/mascota1.png',
+  '/assets/img/mascota4.png',
+  '/assets/img/mascota5.png'
+];
+
+activeIndex: number = 0; // Índice de la imagen activa
+
+nextImage() {
+  this.activeIndex = (this.activeIndex + 1) % this.images.length;
+}
+
+prevImage() {
+  this.activeIndex = (this.activeIndex - 1 + this.images.length) % this.images.length;
+}
 }
